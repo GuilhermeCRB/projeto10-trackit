3a1cde3dfe5871bfa695f67f73bdcf1ad3c7b342
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-04-04 11:04:37 -0300

    feat: displaying history screen

--- src/components/HistoryScreen.jsx
+++ src/components/HistoryScreen.jsx
@@ -1,5 +1,41 @@
+import styledComponents from "styled-components";
+
+import Header from "./Header";
+import Menu from "./Menu";
+
 export default function HistoryScreen(){
-    return(
-        "History"
+    return (
+        <section>
+            <Header />
+            <MainStyle>
+                <h2>Histórico</h2>
+                <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+            </MainStyle>
+            <Menu />
+        </section>
     );
 }
+
+
+const MainStyle = styledComponents.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: calc(100vh - 140px);
+    margin: 70px 0;
+    background-color: var(--background-screen);
+
+    h2{
+        font-size: 24px;
+        width: 90vw;
+        margin-top: 28px;
+        color: var(--h2);
+    }
+
+    p{
+        font-size: 18px;
+        width: 90vw;
+        margin-top: 17px;
+        color: var(--text2);
+    }
+`
